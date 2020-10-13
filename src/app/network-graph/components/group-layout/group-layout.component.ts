6dc26fa10cd113208381d2777965aa6702bd8cc8
sumitkumar25 <sumitsdkumar@gmail.com>
2020-10-14 00:39:32 +0530

    group layout base.

--- src/app/network-graph/components/group-layout/group-layout.component.ts
+++ src/app/network-graph/components/group-layout/group-layout.component.ts
@@ -1,3 +1,7 @@
+/**
+ * TODO: check if other possibilities are there for loaded mesh. current 'Group'.
+ * TODO: Groups Texture.
+ */
 import {
   AfterViewInit,
   Component,
@@ -5,10 +9,11 @@ import {
   OnInit,
   ViewChild,
 } from "@angular/core";
+import { forkJoin } from "rxjs";
 import { ThreeService } from "src/app/three/services/three.service";
 import * as THREE from "three";
 import * as Stats from "../../../../../node_modules/stats.js/build/stats.min.js";
-import { OBJLoader } from "./../../../../../node_modules/three/examples/jsm/loaders/OBJLoader";
+import { NetworkGraphRequestService } from "../../services/network-graph-request.service.js";
 
 @Component({
   selector: "app-group-layout",
@@ -31,14 +36,30 @@ export class GroupLayoutComponent implements OnInit, AfterViewInit {
   renderCalls: any;
   objectCount: number = 3;
   layoutMesh: THREE.Mesh<THREE.CircleGeometry, THREE.MeshBasicMaterial>;
-    nodeGeometry: THREE.InstancedBufferGeometry | THREE.BufferGeometry;
-    nodeMesh: THREE.InstancedMesh<any, THREE.MeshNormalMaterial>;
+  nodeMesh: THREE.InstancedMesh<any, THREE.MeshNormalMaterial | any>;
+  instancedNodeMesh: THREE.InstancedMesh<
+    THREE.InstancedBufferGeometry | THREE.BufferGeometry,
+    THREE.MeshBasicMaterial
+  >;
 
-  constructor(private threeService: ThreeService) {}
+  // Level 1 Response;
+  groupsData: Object;
+
+  // connections
+
+  constructor(
+    private threeService: ThreeService,
+    private graphRequestService: NetworkGraphRequestService
+  ) {}
+
+  ngOnInit() {
+    this.initRequests();
+  }
 
   ngAfterViewInit(): void {
     this.threeCommon = this.threeService.getThreeCommon(
-      this.canvasEl.nativeElement
+      this.canvasEl.nativeElement,
+      true
     );
     this.setUpStats();
     this.setUpHelpers();
@@ -47,8 +68,54 @@ export class GroupLayoutComponent implements OnInit, AfterViewInit {
       this.canvasEl.nativeElement.offsetWidth /
       this.canvasEl.nativeElement.offsetHeight;
     this.threeCommon.renderer.setPixelRatio(window.devicePixelRatio);
-    // this.threeCommon.scene.background = 'black';
+    this.threeCommon.scene.background = "black";
     this.threeCommon.camera.updateProjectionMatrix();
+  }
+
+  initRequests() {
+    forkJoin(
+      this.graphRequestService.getGroups(),
+      this.graphRequestService.getNodeMesh()
+    ).subscribe(([groupData, mesh]) => {
+      this.nodeMeshResponseHandler(mesh);
+      this.groupsResponseHandler(groupData);
+    });
+  }
+
+  nodeMeshResponseHandler(mesh) {
+    if (mesh.type && mesh.type === "Group") {
+      this.nodeMesh = mesh.children[0];
+    } else {
+      this.nodeMesh = mesh;
+    }
+  }
+
+  groupsResponseHandler(groupData: Object) {
+    this.groupsData = groupData;
+    this.objectCount = this.groupsData["data"].length;
+    this.initLayout();
+    this.constructNodes();
+    this.renderView();
+  }
+
+  initLayout() {
+    this.threeService.cleanScene(this.threeCommon);
+    const radiusOffset = Math.floor(this.objectCount / 10);
+    this.layoutGeometry = new THREE.CircleGeometry(
+      10 + radiusOffset * radiusOffset,
+      this.objectCount,
+      0,
+      2 * Math.PI
+    );
+    this.layoutMaterial = new THREE.MeshPhongMaterial({
+      color: "white",
+      transparent: true,
+      opacity: 0.3,
+    });
+    this.layoutMesh = new THREE.Mesh(this.layoutGeometry, this.layoutMaterial);
+    // this.layoutMesh.rotateX(-1);
+    // this.layoutMesh.upda
+    this.threeCommon.scene.add(this.layoutMesh);
   }
 
   setUpHelpers() {
@@ -56,17 +123,17 @@ export class GroupLayoutComponent implements OnInit, AfterViewInit {
   }
 
   constructNodes() {
-    const material = new THREE.MeshNormalMaterial();
-    const matrix = new THREE.Matrix4();
-    this.nodeMesh = new THREE.InstancedMesh(
-      this.nodeGeometry,
-      material,
+    this.instancedNodeMesh = new THREE.InstancedMesh(
+      this.nodeMesh.geometry,
+      this.nodeMesh.material,
       this.objectCount
     );
+    console.log(this.instancedNodeMesh);
     for (var i = 0; i < this.objectCount; i++) {
-      this.nodeMesh.setMatrixAt(i, this.setPositionFromLayout(i));
+      //using indexes as id.
+      this.instancedNodeMesh.setMatrixAt(i, this.setPositionFromLayout(i));
     }
-    this.threeCommon.scene.add(this.nodeMesh);
+    this.threeCommon.scene.add(this.instancedNodeMesh);
   }
 
   setPositionFromLayout(i: number): THREE.Matrix4 {
@@ -76,106 +143,58 @@ export class GroupLayoutComponent implements OnInit, AfterViewInit {
     var quaternion = new THREE.Quaternion();
     var scale = new THREE.Vector3();
     if (!this.layoutGeometry.vertices[i]) {
-      console.log("no vertex for index", i+1);
+      console.log("no vertex for index", i + 1);
     }
-    position = this.layoutGeometry.vertices[i+1];
-    // rotation.x = Math.random() * 2 * Math.PI;
-    // rotation.y = Math.random() * 2 * Math.PI;
-    // rotation.z = Math.random() * 2 * Math.PI;
-
+    position = this.layoutGeometry.vertices[i + 1];
     quaternion.setFromEuler(rotation);
-
     scale.x = scale.y = scale.z = 1;
-
     matrix.compose(position, quaternion, scale);
     return matrix;
-  }
-
-  initLayout() {
-    this.threeService.cleanScene(this.threeCommon);
-    this.layoutGeometry = new THREE.CircleGeometry(
-      10,
-      this.objectCount,
-      0,
-      2 * Math.PI
-    );
-    this.layoutMaterial = new THREE.MeshBasicMaterial({
-      color: "black",
-      wireframe: true,
-    });
-    this.layoutMesh = new THREE.Mesh(this.layoutGeometry, this.layoutMaterial);
-    // this.layoutMesh.rotateX(-1);
-    // this.layoutMesh.upda
-    this.threeCommon.scene.add(this.layoutMesh);
   }
 
   setUpStats() {
     this.stats = new Stats();
     this.stats.showPanel(1); // 0: fps, 1: ms, 2: mb, 3+: custom
     this.statsEl.nativeElement.appendChild(this.stats.dom);
-    this.stats.domElement.style.position = "absolute";
-    this.stats.domElement.style.left = "0px";
-    this.stats.domElement.style.top = "0px";
+    this.stats.update();
   }
 
-  clickHandler($event) {
-
+  updateStats() {
+    window.requestAnimationFrame(this.updateStats.bind(this));
   }
+
+  clickHandler($event) {}
   private renderView() {
-    // if (this.mouse) {
-    //   this.raycaster.setFromCamera(this.mouse, this.threeCommon.camera);
-    // }
-
-    // var intersection = this.raycaster.intersectObject(this.mesh);
-    // if (intersection.length > 0) {
-    //   var rotationMatrix = new THREE.Matrix4().makeRotationY(0.2);
-    //   var instanceMatrix = new THREE.Matrix4();
-    //   var instanceId = intersection[0].instanceId;
-    //   const matrix = new THREE.Matrix4();
-    //   this.mesh.getMatrixAt(intersection[0].instanceId, instanceMatrix);
-    //   matrix.multiplyMatrices(instanceMatrix, rotationMatrix);
-
-    //   this.mesh.setMatrixAt(intersection[0].instanceId, matrix);
-    //   this.mesh.instanceMatrix.needsUpdate = true;
-    // }
     this.threeCommon.renderer.render(
       this.threeCommon.scene,
       this.threeCommon.camera
     );
     this.threeCommon.controls.update();
-    this.stats.update();
     window.requestAnimationFrame(this.renderView.bind(this));
+    this.stats.update();
     this.renderCalls = this.threeCommon.renderer.info.render.calls;
   }
 
   objectCountChangeHandler($event) {
+    this.threeService.cleanScene(this.threeCommon);
     this.objectCount = Number.parseInt($event.target.value, 10);
     this.sceneController();
   }
 
-    private sceneController() {
-        this.initLayout();
-        this.constructNodes();
-        this.renderView();
-    }
-
-  ngOnInit() {
-    this.loadGeometry();
+  private sceneController() {
+    this.initLayout();
+    this.constructNodes();
+    this.constructConnection();
+    this.renderView();
   }
-
-  loadGeometry() {
-    const loader = new THREE.BufferGeometryLoader();
-    loader.load("assets/custom/geometry2.json", (geometry) => {
-      this.nodeGeometry = geometry;
-
-    });
-    const objLoader = new OBJLoader();
-    objLoader
-      .loadAsync("assets/node-spheres/sphere.obj")
-      .then((res) => {
-        this.nodeGeometry = res.children[0].geometry;
-        this.sceneController();
-      })
-      .catch((e) => console.log(e));
+  constructConnection() {
+    var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+    var points = [];
+    points.push(this.layoutMesh.geometry);
+    points.push(new THREE.Vector3(0, 10, 0));
+    points.push(new THREE.Vector3(10, 0, 0));
+    var geometry = new THREE.BufferGeometry().setFromPoints(points);
+    var line = new THREE.Line(geometry, material);
+    this.threeCommon.scene.add(line);
   }
 }
